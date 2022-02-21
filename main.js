f453e9ae0f2c7af517dd6da2a4242914a07a0afa
Demmie-bit <77945051+Demmie-bit@users.noreply.github.com>
2022-02-21 09:56:05 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function take_snapshot() {
 }
 console.log('ml5 version', ml5.version);
 
-Classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GLCEFmOca/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GLCEFmOca/model.json', modelLoaded);
 
 function modelLoaded(){
     console.log('model  Loaded');
